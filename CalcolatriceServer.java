740029f7f57babffc411d1f97e1125a99c406cbe
Laerte-7 <laerterussillo@gmail.com>
2025-11-26 12:09:38 +0100

    Update CalcolatriceServer.java

--- CalcolatriceServer.java
+++ CalcolatriceServer.java
@@ -8,31 +8,31 @@ public class CalcolatriceServer {
         try (ServerSocket serverSocket = new ServerSocket(PORTA)) {
     System.out.println("Server avviato sulla porta " + PORTA);
    
-         
-      }
-    }
          while (true) {
-    Socket client = serverSocket.accept();
+            Socket client = serverSocket.accept();
     
       }
       
      
         String[] parti = richiesta.split(" ");
         if (parti.length != 3) {
-          return "ERRORE: Formato non valido";
+              return "ERRORE: Formato non valido";
         }
         double num1 = Double.parseDouble(parti[0]);
         String operazione = parti[1];
         double num2 = Double.parseDouble(parti[2]);
         switch (operazione) {
-          case "+": return num1 + num2;
-          case "-": return num1 - num2;
-          case "*": return num1 * num2;
-          case "/": 
+              case "+": return num1 + num2;
+              case "-": return num1 - num2;
+              case "*": return num1 * num2;
+              case "/": 
         if (num2 == 0) throw new ArithmeticException("Divisione per zero");
-          return num1 / num2;
-        default: throw new IllegalArgumentException("Operazione non supportata");
+              return num1 / num2;
+              default: throw new IllegalArgumentException("Operazione non supportata");
         }
+      }
+    }
+         
       
     
 
